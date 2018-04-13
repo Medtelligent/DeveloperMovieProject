7ca38c38281f36ca658989d629deb9cd7d940e93
stvn100 <31903922+stvn100@users.noreply.github.com>
2018-04-13 10:00:01 -0500

    Initial commit
    ASP.NET app basic setup, authentication enabled

---
+++ DeveloperMovieProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DeveloperMovieProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
